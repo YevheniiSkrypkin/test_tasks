191ae3fcb9fd2df473dd3371706e567e5e4aaea5
YevheniiSkrypkin <81817746+YevheniiSkrypkin@users.noreply.github.com>
2022-01-27 13:16:38 +0200

    Add files via upload

---
+++ task/js/script.js
@@ -0,0 +1,16 @@
+if(window.matchMedia('(min-width: 769px)').matches){
+    $(document).ready(function(){       
+        $('.team-image').slick({
+            dots: true,
+            infinite: true,
+            speed: 700,
+            autoplay:true,
+            autoplaySpeed: 2000,
+            arrows:true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            
+        })})}
+
+
+
